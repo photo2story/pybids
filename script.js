2c903fcee33f3331a39fdd8f72c42d9f2227fedc
photo2story <photo2story@naver.com>
2024-06-16 02:38:53 +0900

    css

--- script.js
+++ script.js
@@ -80,14 +80,20 @@ document.addEventListener('DOMContentLoaded', () => {
                     window.open(item[linkKey], '_blank'); // 새 창에서 링크 열기
                 }
             };
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.style.marginRight = '10px';
+            checkbox.style.accentColor = 'yellow'; // 노란색으로 체크박스 색상 변경
             const date = item[dateKey] ? item[dateKey].split(' ')[0] : '';
             let extraInfo = extraKey ? `<br>낙찰자: ${item[extraKey]}` : '';
             task.innerHTML = `<span>${date} ${item[key]}${extraInfo}</span>`;
+            task.prepend(checkbox); // 체크박스를 태스크 앞에 추가
             container.appendChild(task);
         });
         console.log('Displayed data:', items);
     }
 });
+
 
 
 
