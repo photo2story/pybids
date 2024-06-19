abb999cd39dbee046bb5ae9b12c781f41c212a40
photo2story <photo2story@naver.com>
2024-06-19 15:54:55 +0900

    discord

--- app.py
+++ app.py
@@ -2,10 +2,7 @@
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from threading import Thread
 import os
-import pandas as pd
-from dotenv import load_dotenv
 import subprocess
 import json
 import requests
@@ -71,11 +68,9 @@ def get_data():
     )
     return response
 
-def run_flask():
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)), use_reloader=False)
-
 if __name__ == "__main__":
-    run_flask()
+    app.run(host='127.0.0.1', port=int(os.getenv('PORT', 8080)), use_reloader=False)
+
 
 # .\\.venv\\Scripts\\activate
 # python app.py
